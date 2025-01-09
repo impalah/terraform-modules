1e44b44f3d3c097195dc99b1b142b92e5b2293b5
Lino Figueroa <impalah@gmail.com>
2025-01-09 13:59:13 +0100

    Added stage message

--- api-gateway-http/outputs.tf
+++ api-gateway-http/outputs.tf
@@ -2,3 +2,8 @@ output "api_gateway_id" {
   value = aws_apigatewayv2_api.api.id
 }
 
+output "api_gateway_stage_message" {
+  value = "aws [--profile my_profile] apigatewayv2 create-stage --region ${data.aws_region.current.name} --auto-deploy --api-id ${aws_apigatewayv2_api.api.id} --stage-name '$default'"
+}
+
+data "aws_region" "current" {}
