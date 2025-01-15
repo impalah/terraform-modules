2912be52d8cf5ba1c747c5475db688fd7f73627c
Lino Figueroa <impalah@gmail.com>
2025-01-15 16:54:19 +0100

    Improve app runner

--- app-runner/main.tf
+++ app-runner/main.tf
@@ -3,6 +3,76 @@
 # ###################################################################################
 
 
+resource "aws_iam_role" "app_runner_access_role" {
+  name = "${var.app_name}-app-runner-access-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "build.apprunner.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "app_runner_access_policy_ecr" {
+  role       = aws_iam_role.app_runner_access_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# resource "aws_iam_role_policy" "app_runner_access_policy_custom" {
+#   name = "app-runner-access-policy-custom"
+#   role = aws_iam_role.app_runner_access_role.id
+
+#   policy = var.access_role_policy
+
+# }
+
+
+resource "aws_iam_role" "app_runner_instance_role" {
+  name = "${var.app_name}-app-runner-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "tasks.apprunner.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "app_runner_instance_ecr_policy" {
+  role       = aws_iam_role.app_runner_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "app_runner_instance_ec2_policy" {
+  role       = aws_iam_role.app_runner_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+
+resource "aws_iam_role_policy" "app_runner_instance_policy" {
+  name = "${var.app_name}-app-runner-instance-policy"
+  role = aws_iam_role.app_runner_instance_role.id
+
+  policy = var.instance_role_policy
+
+}
+
+
+
+
 resource "aws_security_group" "app_runner_sg" {
   name        = "${var.app_name}-sg"
   description = "Security group for App Runner VPC Connector"
@@ -54,17 +124,31 @@ resource "aws_apprunner_vpc_connector" "app_vpc_connector" {
 resource "aws_apprunner_service" "app_service" {
   service_name = var.app_name
 
+  instance_configuration {
+    instance_role_arn = aws_iam_role.app_runner_instance_role.arn
+    cpu               = var.cpu
+    memory            = var.memory
+  }
+
   source_configuration {
+
+    authentication_configuration {
+      access_role_arn = aws_iam_role.app_runner_access_role.arn
+    }
+
     image_repository {
       image_identifier      = var.ecr_image_uri
       image_repository_type = "ECR"
 
       image_configuration {
+        port                          = var.port
         runtime_environment_variables = var.environment_variables
         start_command                 = var.start_command
       }
+
+
     }
-    auto_deployments_enabled = true
+    auto_deployments_enabled = var.auto_deployments_enabled
   }
 
   network_configuration {
@@ -72,6 +156,18 @@ resource "aws_apprunner_service" "app_service" {
       egress_type       = "VPC"
       vpc_connector_arn = aws_apprunner_vpc_connector.app_vpc_connector.arn
     }
+    ingress_configuration {
+      is_publicly_accessible = var.is_publicly_accessible
+    }
+  }
+
+  health_check_configuration {
+    path                = var.health_check_configuration.path
+    protocol            = var.health_check_configuration.protocol
+    interval            = var.health_check_configuration.interval
+    timeout             = var.health_check_configuration.timeout
+    healthy_threshold   = var.health_check_configuration.healthy_threshold
+    unhealthy_threshold = var.health_check_configuration.unhealthy_threshold
   }
 
   tags = merge(
--- app-runner/variables.tf
+++ app-runner/variables.tf
@@ -48,3 +48,75 @@ variable "start_command" {
   type        = string
   default     = null
 }
+
+variable "access_role_policy" {
+  description = "App Runner access role policy"
+  type        = string
+  default     = null
+}
+
+variable "instance_role_policy" {
+  description = "App Runner instance role policy"
+  type        = string
+  default     = null
+}
+
+variable "port" {
+  description = "Port where the container is exposed"
+  type        = string
+  default     = "8080"
+}
+
+variable "auto_deployments_enabled" {
+  description = "Enable auto deployments"
+  type        = bool
+  default     = true
+}
+
+variable "is_publicly_accessible" {
+  description = "Is the service publicly accessible"
+  type        = bool
+  default     = true
+}
+
+variable "cpu" {
+  description = "Number of CPU units reserved for each instance of your App Runner service represented as a String. Defaults to 1024. Valid values: 256|512|1024|2048|4096|(0.25|0.5|1|2|4) vCPU."
+  type        = string
+  default     = "1024"
+
+  validation {
+    condition     = can(regex("^(256|512|1024|2048|4096|0.25|0.5|1|2|4)$", var.cpu))
+    error_message = "The cpu variable must be one of the following values: 256, 512, 1024, 2048, 4096, 0.25, 0.5, 1, 2, 4."
+  }
+}
+
+variable "memory" {
+  description = "Amount of memory, in MB or GB, reserved for each instance of your App Runner service. Defaults to 2048. Valid values: 512|1024|2048|3072|4096|6144|8192|10240|12288|(0.5|1|2|3|4|6|8|10|12) GB."
+  type        = string
+  default     = "2048"
+
+  validation {
+    condition     = can(regex("^(512|1024|2048|3072|4096|6144|8192|10240|12288|0.5|1|2|3|4|6|8|10|12)$", var.memory))
+    error_message = "The memory variable must be one of the following values: 512, 1024, 2048, 3072, 4096, 6144, 8192, 10240, 12288, 0.5, 1, 2, 3, 4, 6, 8, 10, 12."
+  }
+}
+
+variable "health_check_configuration" {
+  description = "Configuration for health checks"
+  type = object({
+    path                = string
+    protocol            = string
+    interval            = number
+    timeout             = number
+    healthy_threshold   = number
+    unhealthy_threshold = number
+  })
+  default = {
+    path                = "/"
+    protocol            = "TCP"
+    interval            = 10
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+}
