2e98a8f934005125eec439d5b1c25c9bc2eab658
Lino Figueroa <impalah@gmail.com>
2025-01-17 14:22:31 +0100

    Updated ACM

--- acm/main.tf
+++ acm/main.tf
@@ -1,9 +1,13 @@
 resource "aws_acm_certificate" "cert" {
-  count             = var.domain_name != null ? 1 : 0
-  domain_name       = var.domain_name
-  validation_method = "DNS"
+  count                     = var.domain_name != null ? 1 : 0
+  domain_name               = var.domain_name
+  validation_method         = "DNS"
   subject_alternative_names = var.subject_alternative_names
-  tags              = var.tags
+
+  tags = merge(
+    var.tags,
+    var.default_tags,
+  )
 
   lifecycle {
     create_before_destroy = true
@@ -22,14 +26,14 @@ resource "aws_route53_record" "cert_validation" {
 
   lifecycle {
     create_before_destroy = true
-    prevent_destroy = true
+    prevent_destroy       = true
   }
 }
 
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  count                = var.domain_name != null ? 1 : 0
-  certificate_arn      = aws_acm_certificate.cert[0].arn
+  count                   = var.domain_name != null ? 1 : 0
+  certificate_arn         = aws_acm_certificate.cert[0].arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
@@ -38,7 +42,11 @@ resource "aws_acm_certificate" "import_cert" {
   private_key       = var.private_key
   certificate_body  = var.certificate_body
   certificate_chain = var.certificate_chain
-  tags              = var.tags
+
+  tags = merge(
+    var.tags,
+    var.default_tags,
+  )
 
   lifecycle {
     create_before_destroy = true
